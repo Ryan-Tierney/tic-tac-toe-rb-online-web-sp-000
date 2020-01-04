303a06f9e0b36a0d33523be7df1fc41749d27c57
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-01-04 03:58:27 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -12,8 +12,8 @@ WIN_COMBINATIONS = [
     puts "display_board"
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts "-----------"
-    puts " {board[3]} | #{board[4]} | #{board[5]} "
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
     puts "-----------"
-    puts " {board[6]} | #{board[7]} | #{board[8]} "
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
   end 
   
