309cf9707bcefb87ea33c875fe200ccf3d769e29
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-01-04 20:13:36 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -58,7 +58,14 @@ def turn_count(board)
 end
 
 def current_player(board)
-  
+  if turn_count(board)%2 ==0
+    current_player = "X"
+  else
+    current_player = "O"
+end
+return current_player
+end
+
 end
 
 def won?(board)
