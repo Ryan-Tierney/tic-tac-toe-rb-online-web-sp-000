fb4ebf15ce4a338148d6588ae3c9b471498d9e03
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-01-04 04:14:57 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,7 +27,7 @@ def move(board, index, token)
 end 
     
 def position_taken?(board, index)
-  if board[index] = " " || board[index] = ""
+  if board[index] = nil || board[index] = " "
     false 
   else
     true
