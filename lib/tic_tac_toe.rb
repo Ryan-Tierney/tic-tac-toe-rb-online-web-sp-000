c839c5e88c318442d6dd10805e1668dc61e5368c
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-01-04 20:49:35 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,5 +1,3 @@
-
-
 WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
@@ -9,65 +7,55 @@ WIN_COMBINATIONS = [
   [2,5,8],
   [0,4,8],
   [2,4,6]]
-
-
-#OUTPUT BOARD
-def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
-puts 'display board'
-#board output
-puts " #{board[0]} | #{board[1]} | #{board[2]} "
-puts "-----------"
-puts " #{board[3]} | #{board[4]} | #{board[5]} "
-puts "-----------"
-puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-#CHANGE INFO TO INTEGER
-def input_to_index(move)
-   index = move.to_i - 1
-   index
-end
-
-#MAKE TURN
-def turn(board)
-  puts "Please enter 1-9:"
-  #get the user input
-  user_input = gets.strip
-  #input to index
-  index = input_to_index(user_input)
-  token = current_player(board)
   
-  #check for validation
-  if valid_move?(board,index)
-    puts 'valid move'
-    move(board, index, token)
-    display_board(board)
-   else
-    puts 'try again'
-    turn(board)
-  end
-  display_board(board)
-end
-
-#CHECK FOR EMPTY SPACE
+def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+    puts "display_board"
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end 
+  
+def input_to_index(move)
+    index = move.to_i - 1 
+    index 
+end 
+  
+def move(board, index, token)
+  board[index] = token
+end 
+    
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
-end
-
-#IS # CORRECT & SPACE EMPTY?
+  end 
+ 
 def valid_move?(board, index)
-
   if index.between?(0,8) && !position_taken?(board, index)
-      puts 'this is a valid move'
-    return true
+    return true 
   else
-   return false
-  end
+    return false 
+  end 
 end
 
-#COUNTER FOR PLAYER ASSESMENT
+def turn(board)
+  puts "take yo turn bitch"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  token = current_player(board)
+  if valid_move?(board, index)
+    puts "I guess thats a good move, chump"
+    move(board, index, token)
+    display_board(board)
+  else 
+    puts "Nuh uh Motha fucka"
+    turn(board)
+  end 
+  display_board(board)
+end 
+
 def turn_count(board)
-  counter = 0
+   counter = 0
   board.each do |space|
     if space == "X" || space == "O"
       counter +=1
@@ -76,9 +64,8 @@ end
 return counter
 end
 
-#WHICH PLAYER IS THIS?
 def current_player(board)
-  if turn_count(board)%2 ==0
+  if turn_count(board) % 2 == 0
     current_player = "X"
   else
     current_player = "O"
@@ -86,57 +73,31 @@ end
 return current_player
 end
 
-
-#MAKE MOVE ON BOARD
-def move(board, index, token)
-  board[index] = token
-  play(board)
-  
-end
-
-
-
-
-
-#HAS ANYONE WON? return false if no. return win_combo if yes
 def won?(board)
-  WIN_COMBINATIONS.each do |win_combo|
-    #check for player 1 win
-    if check_win_combination?(board, 'X', win_combo)
-      return win_combo
-      #check for player 2 win
-    elsif check_win_combination?(board, 'O', win_combo)
-      return win_combo
-    else
-      return false
-    end
-  end
-end
+   if WIN_COMBINATIONS
+     return true 
+   elsif draw?(board) 
+     return false 
+  end 
+end 
+  
 
-#CHECK WINNING COMBOS
-def check_win_combination?(board, player, win_combo)
-  win_combo.all? do |position|
-    board[position] == player
-  end
-end
 
-#IS BOARD FULL? 
+
 def full?(board)
-  if board.include?(' ') || board.include?('')
+   if board.include?(' ') || board.include?('')
     return false
 else
     return true
   end
-end
+end 
 
-#IS THERE A DRAW?
 def draw?(board)
   if !won?(board) && full?(board)
     return true
   end
 end
 
-#HAS SOMETHING HAPPENED?
 def over?(board)
   puts 'is it over?'
   if won?(board) || draw?(board) || full?(board)
@@ -147,7 +108,6 @@ def over?(board)
   end
 end
 
-#WHO WON?
 def winner(board)
   if !won?(board)
     return nil
@@ -160,7 +120,6 @@ def winner(board)
   end
 end
 end
-
 
 def play(board)
   until over?(board) == true || won?(board) != false
@@ -175,11 +134,3 @@ def play(board)
     return nil
   end
 end
-
-
-
-
-
-
-display_board(board)
-play(board)
