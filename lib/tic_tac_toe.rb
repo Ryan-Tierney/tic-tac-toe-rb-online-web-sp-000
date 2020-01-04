175b7f54fcbedb3c55e6816959914dc01c9d890f
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-01-04 20:18:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -55,7 +55,13 @@ def turn(board)
 end 
 
 def turn_count(board)
-  
+   counter = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      counter +=1
+  end
+end
+return counter
 end
 
 def current_player(board)
