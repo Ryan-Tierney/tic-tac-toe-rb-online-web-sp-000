9547c54064673783f31d2221c61ad64859a017e8
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-01-04 04:07:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -8,23 +8,25 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [2,4,6]]
   
-  def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
     puts "display_board"
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts "-----------"
     puts " #{board[3]} | #{board[4]} | #{board[5]} "
     puts "-----------"
     puts " #{board[6]} | #{board[7]} | #{board[8]} "
-  end 
+end 
   
-  def input_to_index(move)
+def input_to_index(move)
     index = move.to_i - 1 
     index 
-  end 
+end 
   
-  def move(board, index, token)
-    board(index) = token
+def move(board, index, token)
+  board[index] = token
+  play(board)
+end 
     
-  end 
+
     
     
