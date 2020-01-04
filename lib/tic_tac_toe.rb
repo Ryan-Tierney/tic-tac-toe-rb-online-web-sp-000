df77bc458df011051d5d6f5911aaf900d43a5dc7
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-01-04 03:56:06 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -8,5 +8,7 @@ WIN_COMBINATIONS = [
   [0,4,8]
   [2,4,6]]
   
-  def display_board(board) = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+    puts display_board
+  end 
   
